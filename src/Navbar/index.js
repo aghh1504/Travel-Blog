072303a1531cd2b8fa5d66fa624dc4132f91f645
agatta1504 <agatta1504@interia.pl>
2017-10-19 22:08:23 +0100

    first commit

---
+++ src/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img className='navbar__logo' src='http://www.freeiconspng.com/uploads/-earth-find-flight-fly-globe-map-travel-icon--icon-search-engine-4.png'/>
+      <div className='navbar__name'>Do things --> Travel</div>
+    </div>
+  )
+}
+
+export default Navbar;
